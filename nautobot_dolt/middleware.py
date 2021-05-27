b1089196a5eaea91a643c5c49c074b693d0af913
Andy Arthur <andy@dolthub.com>
2021-05-26 19:49:59 -0700

    added current branch banner and cleaned up session state

--- nautobot_dolt/middleware.py
+++ nautobot_dolt/middleware.py
@@ -1,4 +1,6 @@
+from django.contrib import messages
 from django.shortcuts import redirect
+from django.utils.safestring import mark_safe
 
 from nautobot_dolt.constants import (
     DOLT_BRANCH_KEYWORD,
@@ -13,29 +15,42 @@ class DoltMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        if self._redirect_needed(request):
+        # check for a `branch` query string param
+        branch = request.GET.get(DOLT_BRANCH_KEYWORD, None)
+
+        if branch:
+            # update the session cookie with the current branch
+            request.session[DOLT_BRANCH_KEYWORD] = branch
+        elif self._is_vcs_route(request):
+            # route is under version control, but no branch was specified,
+            # lookup the current branch in the session cookie.
             branch = request.session.get(DOLT_BRANCH_KEYWORD, DOLT_DEFAULT_BRANCH)
+            # provide the `branch` query string param and redirect
             return redirect(f"{request.path}?{DOLT_BRANCH_KEYWORD}={branch}")
 
         return self.get_response(request)
 
     @staticmethod
-    def _redirect_needed(request):
+    def _is_vcs_route(request):
         """
-        Versioned domain pages will be redirected if they
-        do not contain a `dolt_branch` query string param.
+        Determines whether the requested page is under version-control
+        and needs to be redirected to read from a specific branch.
         """
         if request.GET.get(DOLT_BRANCH_KEYWORD, None):
+            # if a branch is already specified in the
+            # query string, don't redirect
             return False
 
-        # query string param not found
         return (
             request.path.startswith(DOLT_VERSIONED_URL_PREFIXES) or request.path == "/"
-        )  # home page
+        )
 
     def process_view(self, request, view_func, view_args, view_kwargs):
-        branch = request.GET.get(DOLT_BRANCH_KEYWORD, None)
-        if branch:
-            request.session[DOLT_BRANCH_KEYWORD] = branch
-            Branch.objects.get(pk=branch).checkout_branch()
+        # lookup the current branch in the session cookie
+        branch = request.session.get(DOLT_BRANCH_KEYWORD, DOLT_DEFAULT_BRANCH)
+        # switch the database to use the current branch
+        Branch.objects.get(pk=branch).checkout_branch()
+        # inject the "current branch" banner
+        messages.info(request, mark_safe(f"<h4>current branch: {branch}</h4>"))
+
         return view_func(request, *view_args, **view_kwargs)
