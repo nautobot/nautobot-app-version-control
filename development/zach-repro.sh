2429d4b14d578cf942ed52a0f79a665d16e88189
Andy Arthur <andy@dolthub.com>
2021-08-11 11:34:36 -0700

    cleanup func

--- development/zach-repro.sh
+++ development/zach-repro.sh
@@ -5,16 +5,26 @@ set -eo pipefail
 # verify python 3+
 python --version
 
-# run dolt sql-server in the background
+
+# setup dolt repo and run
+# dolt sql-server in the background
+function cleanup() {
+  rm -rf nautobot
+  kill -9 $PID
+}
+mkdir nautobot
+trap cleanup "EXIT"
+
 cp dolt-config.yaml nautobot/
 pushd nautobot
 dolt init
 dolt sql-server --config dolt-config.yaml &
+PID=$!
 popd
 
 # install poetry package manager
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
-`source $HOME/.poetry/env`
+source $HOME/.poetry/env
 
 # setup poetry environment
 pushd ../
