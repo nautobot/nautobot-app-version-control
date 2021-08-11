b7529355c24398466ee15e4e23c45b908ac1aa0b
Andy Arthur <andy@dolthub.com>
2021-08-11 11:15:21 -0700

    repro script for zach

---
+++ development/zach-repro.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -eo pipefail
+
+# verify python 3+
+python --version
+
+# run dolt sql-server in the background
+cp dolt-config.yaml nautobot/
+pushd nautobot
+dolt init
+dolt sql-server --config dolt-config.yaml &
+popd
+
+# install poetry package manager
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+
+# setup poetry environment
+pushd ../
+poetry install
+
+# enter poetry environment
+poetry shell
+
+# run the inital django db migrations
+nautobot-server migrate
+
+
