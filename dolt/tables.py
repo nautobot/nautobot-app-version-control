96775f2329882cd526c1f07085cdb9a45f334344
Vinai Rachakonda <rachakondavinai@gmail.com>
2021-09-10 13:11:50 -0700

    add the starting branch param

--- dolt/tables.py
+++ dolt/tables.py
@@ -45,6 +45,9 @@ class BranchTable(BaseTable):
         buttons=("checkout",),
         prepend_template=BRANCH_TABLE_BADGES,
     )
+    starting_branch = tables.Column(
+        accessor=A("source_branch"), verbose_name="Starting Branch"
+    )
 
     class Meta(BaseTable.Meta):
         model = Branch
@@ -55,9 +58,9 @@ class BranchTable(BaseTable):
             "ahead_behind",
             "created_by",
             "latest_committer",
-            "latest_committer_email",
             "latest_commit_date",
             "latest_commit_message",
+            "starting_branch",
             "actions",
         )
         default_columns = (
@@ -65,8 +68,8 @@ class BranchTable(BaseTable):
             "ahead_behind",
             "created_by",
             "latest_committer",
-            "latest_committer_email",
             "latest_commit_date",
+            "starting_branch",
             "actions",
         )
 
