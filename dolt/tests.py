070673e6763e17c22dfca941c3c39baf7fae69c9
Vinai Rachakonda <rachakondavinai@gmail.com>
2021-09-15 14:24:22 -0700

    add test for branch delete

--- dolt/tests.py
+++ dolt/tests.py
@@ -87,6 +87,7 @@ class TestBranchesApi(APITestCase, APIViewTestCases):
 
 
 class TestPullRequestApi(APITestCase, APIViewTestCases):
+    User.objects.create(username="pr-reviewer", is_superuser=True)
     model = PullRequest
     brief_fields = [
         "title",
@@ -97,10 +98,13 @@ class TestPullRequestApi(APITestCase, APIViewTestCases):
         "creator",
         "created_at",
     ]
+    create_data = [
+        {"title": "Review 4", "state": "b1", "source_branch": "b1", "destination_branch": "b2", description: "", creator: User.objects.get(username="pr-reviewer")},
+        {"title": "Review 5", "state": "b1", "source_branch": "b1", "destination_branch": "b2", description: "", creator: User.objects.get(username="pr-reviewer")},
+    ]
 
     @classmethod
     def setUpTestData(cls):
-        User.objects.create(username="pr-reviewer", is_superuser=True)
         PullRequest.objects.create(
             title="Review 1",
             state=0,
