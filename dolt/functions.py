13f7343eb96dd5b708f71ef830a9aef708b69667
Vinai Rachakonda <rachakondavinai@gmail.com>
2021-09-23 18:23:05 -0700

    fix functions.pys

--- dolt/functions.py
+++ dolt/functions.py
@@ -1,10 +1,15 @@
-from django.db import NotSupportedError
+"""
+Functions.py represents custom SQL functions that do not fit the django orm
+"""
+
 from django.db.models.expressions import Func, Value
-from django.db.models import TextField
 from django.db.models.fields.json import JSONField
 
 
+#pylint: disable=W0223
 class JSONObject(Func):
+    """ JSonObject represents the json_object mysql function """
+
     function = "JSON_OBJECT"
     output_field = JSONField()
 
@@ -14,5 +19,6 @@ class JSONObject(Func):
             expressions.extend((Value(key), value))
         super().__init__(*expressions)
 
-    def as_sql(self, compiler, connection, **extra_context):
-        return super().as_sql(compiler, connection, **extra_context)
+    def as_sql(self, compiler, connection, function=None, template=None, **extra_context):
+        """ overrides Func class as_sql """
+        return super().as_sql(compiler, connection, function, template, **extra_context)
