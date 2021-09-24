5531546fc34d1f9d03050812ac0584223f47e949
Vinai Rachakonda <rachakondavinai@gmail.com>
2021-09-23 18:35:02 -0700

    additional functions.py change

--- dolt/functions.py
+++ dolt/functions.py
@@ -6,7 +6,7 @@ from django.db.models.expressions import Func, Value
 from django.db.models.fields.json import JSONField
 
 
-#pylint: disable=W0223
+# pylint: disable=W0223
 class JSONObject(Func):
     """ JSonObject represents the json_object mysql function """
 
@@ -18,7 +18,3 @@ class JSONObject(Func):
         for key, value in fields.items():
             expressions.extend((Value(key), value))
         super().__init__(*expressions)
-
-    def as_sql(self, compiler, connection, function=None, template=None, **extra_context):
-        """ overrides Func class as_sql """
-        return super().as_sql(compiler, connection, function, template, **extra_context)
