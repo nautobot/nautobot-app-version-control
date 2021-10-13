8f9f4aaff38a30aaa583812d49fce47daa00a350
Vinai Rachakonda <rachakondavinai@gmail.com>
2021-10-13 13:22:07 -0700

    add assert

--- dolt/utils.py
+++ dolt/utils.py
@@ -53,6 +53,7 @@ def db_for_commit(commit):
     e.g. "nautobot/3a5mqdgao8029bf8ji0huobbskq1n1l5"
     """
     cm_hash = str(commit)
+    assert len(cm_hash) == 32
     db = deepcopy(connections.databases["default"])
     db["id"] = cm_hash
     db["NAME"] = f"{DB_NAME}/{cm_hash}"
