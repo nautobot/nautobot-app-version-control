322cb8965ad13c39cffbd3368d61da67b5e788df
Bryan Culver <bryan.culver@networktocode.com>
2023-03-08 08:45:06 -0500

    Black and flake8

--- nautobot_version_control/filters.py
+++ nautobot_version_control/filters.py
@@ -25,9 +25,7 @@ class BranchFilterSet(BaseFilterSet):
             "latest_commit_date",
             "latest_commit_message",
         )
-        exclude = [
-            "id"
-        ]
+        exclude = ["id"]
 
     def search(self, queryset, name, value):  # pylint: disable=unused-argument,no-self-use
         """
@@ -67,9 +65,7 @@ class CommitFilterSet(BaseFilterSet):
             "date",
             "message",
         )
-        exclude = [
-            "id"
-        ]
+        exclude = ["id"]
 
     def search(self, queryset, name, value):  # pylint: disable=unused-argument,no-self-use
         """
